d3855520c2a3f29cd870a0a92308a08cad5a3f69
caio20538 <caio20538@gmail.com>
2025-03-26 20:52:53 -0300

    Aplicação RESTful API com java

---
+++ src/test/java/com/dio/restful_com_nuvem/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.restful_com_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
