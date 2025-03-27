91bbf0b4fdec172133fa62ca18eecb3779a14c4c
caio20538 <caio20538@gmail.com>
2025-03-27 18:50:22 -0300

    adicinando a camada controller

--- src/main/java/com/dio/restful_com_nuvem/domain/security/SecurityConfig.java
+++ src/main/java/com/dio/restful_com_nuvem/domain/security/SecurityConfig.java
@@ -1,26 +1,58 @@
-package com.dio.restful_com_nuvem.domain.security;
+//package com.dio.restful_com_nuvem.domain.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//                                "/h2-console/**",
+//                                "/v3/api-docs/**",
+//                                "/swagger-ui/**",
+//                                "/swagger-ui.html"
+//                        ).permitAll() // Permite acesso sem autenticação
+//                        .anyRequest().authenticated() // Protege as demais rotas
+//                )
+//                .csrf(AbstractHttpConfigurer::disable) // Necessário para evitar bloqueios do H2 Console e Swagger
+//                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // Libera iframes do H2 Console
+//
+//        return http.build();
+//    }
+//}
+//
+////{
+////        "name": "caio",
+////        "account": {
+////        "number": "00000-00",
+////        "agency": "000",
+////        "balance": 10000.00,
+////        "limit": 10000.00
+////        },
+////        "features": [
+////        {
+////        "icon": "URL",
+////        "description": "Description of Features"
+////        }
+////        ],
+////        "card": {
+////        "number": "xxxx xxxx xxxx 0000",
+////        "limit": 10000.00
+////        },
+////        "news": [
+////        {
+////        "icon": "URL",
+////        "description": "Description of News"
+////        }
+////        ]
+////        }
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll() // Permite acesso ao H2 Console
-                        .anyRequest().authenticated() // Protege as outras rotas
-                )
-                .csrf(AbstractHttpConfigurer::disable) // Necessário para permitir o H2 Console
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // Necessário para iframes do H2 Console
-
-        return http.build();
-    }
-}
-
+// tratamento de Exceções em APIs REST
